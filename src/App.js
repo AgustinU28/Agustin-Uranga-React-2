42c5bb57df785d071083f9c29fe427023847bf9f
agustinuranga@live.com <agustinuranga@live.com>
2023-01-16 12:36:21 -0300

    modificando

--- src/App.js
+++ src/App.js
@@ -8,28 +8,29 @@ import EffectExample from './components/EffectExample';
 import { useState } from 'react';
 import Animation from './components/Animation';
 
-function App()  
+function App() {
   const [show, setShow] = useState(true)
 
   return (
     <div className="App"> 
       <Animation />
-        { <BrowserRouter>
+        <BrowserRouter>
           <Navbar />
           <Routes>
             <Route path='/' element={<ItemListContainer greeting='Todos nuestro products'/>}/>
             <Route path='/category/:categoryId' element={<ItemListContainer greeting='Productos filtrados'/>} />
             <Route path='/detail/:productId' element={<ItemDetailContainer />} />
-          </Routes> }
-          { <Footer > }
-        { <BrowserRouter> }
-        { <button onClick={() => {setShow(!show)}}>show/hide</button>}
-          { show && <EffectExample> }
-    </div>
-
-        );
+          </Routes>
+          {/* <Footer /> */}
+        </BrowserRouter>
+        <button onClick={() => {setShow(!show)}}>show/hide</button>
+          { show && <EffectExample /> }
+    </div>
+  );
+}
            
         
 
 export default App;
+
         
