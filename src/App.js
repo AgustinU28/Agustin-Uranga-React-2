e02a42ba444fc50b1a9eb3d10b7aea90a2ccc008
agustinuranga@live.com <agustinuranga@live.com>
2023-01-16 13:33:14 -0300

    errores

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import Navbar from './components/Navbar/Navbar';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import EffectExample from './components/EffectExample';
 import { useState } from 'react';
-import Animation from './components/Animation';
+
 
 function App() {
   const [show, setShow] = useState(true)
