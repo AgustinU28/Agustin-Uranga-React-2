d472c1ab4cd0cefeca52446af3c8cc83b4f7b6dd
agustinuranga@live.com <agustinuranga@live.com>
2023-01-17 23:04:31 -0300

    correccion de compu

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -10,7 +10,7 @@ const products = [
     },
     { id: '2', name: 'samsung s22 ULTRA', price: 2000, category: 'celular', img:'https://http2.mlstatic.com/D_NQ_NP_635300-MLA52140990600_102022-O.jpg', stock: 16, description:'Descripcion de Samsung s22 ULTRA'},
     { id: '3', name: 'Tablet 2023', price: 3000, category: 'tablet', img:'https://s1.eestatic.com/2021/12/03/actualidad/631948900_217191977_1024x576.jpg', stock: 10, description:'Descripcion de Tablet 2023'},
-    { id: '4', name: 'MSI GAMER', price: 5000, category: 'Notebooks', img:'https://infoservice-cba.com.ar/wp-content/uploads/2022/07/MSI-GF63-THIN-GAMING-10SCXR-222.jpg', stock: 10, description:'Descripcion de MSI GAMER'},
+    { id: '4', name: 'MSI GAMER', price: 5000, category: 'notebook', img:'https://infoservice-cba.com.ar/wp-content/uploads/2022/07/MSI-GF63-THIN-GAMING-10SCXR-222.jpg', stock: 10, description:'Descripcion de MSI GAMER'},
 ]
 
 
